7a2111ada69b6fa4523861a7806b2a5d6588a805
Valero <valerakochegin2005@gmail.com>
2024-09-19 20:24:29 +0300

    Вроде как починил 2 задание

--- 2/2_2_lab/2_2_lab/2zadan.cpp
+++ 2/2_2_lab/2_2_lab/2zadan.cpp
@@ -1,36 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <Windows.h>
 
 const int size = 100000;
 int randarr1[size], randarr2[size], randarr3[size];
 int vozrast1[size], vozrast2[size], vozrast3[size];
 int ubiv1[size], ubiv2[size], ubiv3[size];
 int vozubiv1[size], vozubiv2[size], vozubiv3[size];
-int vremya[12];
+int k = 0;
+double vremya[12];
 
-void zaparr(int a[], int b[], int c[], int d[], int size)
+void zaparr(int* a, int* b, int* c, int* d, int size)
 {
     int i;
-    srand(time(NULL));
-    for (i=0; i < size; i++) {
+    for (i = 0; i < size; i++) {
         a[i] = rand() % 1000;
         b[i] = i;
         c[i] = size - i;
-        if ((i) < (size / 2)) { d[i] = i; }
+        if (i < (size / 2)) { d[i] = i; }
         else { d[i] = size - i - 1; }
     }
 
 }
 
-void print(int a[], int size)
-{
-    for (int i = 0; i < size; i++) {
-        printf("%d ", a[i]);
-    }
-}
-
-void kopirka(int a[], int b[], int c[], int size)
+void kopirka(int* a, int* b, int* c, int size)
 {
     for (int i = 0; i < size; i++) {
         b[i] = a[i];
@@ -40,7 +36,6 @@ void kopirka(int a[], int b[], int c[], int size)
 
 void shell(int* items, int count)
 {
-
     int i, j, gap, k;
     int x, a[5];
 
@@ -61,9 +56,6 @@ void qs(int* items, int left, int right) //вызов функции: qs(items, 0, count-1);
 {
     int i, j;
     int x, y;
-
-
-
     i = left; j = right;
 
     /* выбор компаранда */
@@ -89,104 +81,67 @@ int compare(const void* a, const void* b) {
     return (*(int*)a - *(int*)b);
 }
 
-int main() {
-    int k = 0;
+void schetShell(int* a, int* k)
+{
+    clock_t start = clock();
+    shell(a, size);
+    clock_t end = clock();
+    vremya[*k] = ((double)(end - start) / CLOCKS_PER_SEC);
+    (*k)++;
+}
 
-    zaparr(&randarr1[size], &vozrast1[size], &ubiv1[size], &vozubiv1[size], size);
+void schetQs(int* a, int* k)
+{
+    clock_t start = clock();
+    qs(a, 0, size - 1);
+    clock_t end = clock();
+    vremya[*k] = ((double)(end - start) / CLOCKS_PER_SEC);
+    (*k)++;
+}
+
+void schetQsort(int* a, int* k)
+{
+    clock_t start = clock();
+    qsort(a, size, sizeof(int), compare);
+    clock_t end = clock();
+    vremya[*k] = ((double)(end - start) / CLOCKS_PER_SEC);
+    (*k)++;
+}
+
+
+int main() {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    srand(time(NULL));
+
+    zaparr(randarr1, vozrast1, ubiv1, vozubiv1, size);
 
     kopirka(&randarr1[size], &randarr2[size], &randarr3[size], size);
     kopirka(&vozrast1[size], &vozrast2[size], &vozrast3[size], size);
-    kopirka(&ubiv1[size], &ubiv2[size], &ubiv2[size], size);
+    kopirka(&ubiv1[size], &ubiv2[size], &ubiv3[size], size);
     kopirka(&vozubiv1[size], &vozubiv2[size], &vozubiv3[size], size);
 
-    clock_t start = clock();
-    shell(randarr1, size);
-    clock_t end = clock();
-    vremya[k] = (double)(end - start) / CLOCKS_PER_SEC;
-    k++;
+    schetShell(randarr1, &k);
+    schetShell(vozrast1, &k);
+    schetShell(ubiv1, &k);
+    schetShell(vozubiv1, &k);
+
+    schetQs(randarr2, &k);
+    schetQs(vozrast2, &k);
+    schetQs(ubiv2, &k);
+    schetQs(vozubiv2, &k);
     
-    start = clock();
-    shell(vozrast1, size);
-    end = clock();
-    vremya[k] = (double)(end - start) / CLOCKS_PER_SEC;
-    k++;
+    schetQsort(randarr3, &k);
+    schetQsort(vozrast3, &k);
+    schetQsort(ubiv3, &k);
+    schetQsort(vozubiv3, &k);
 
-    start = clock();
-    shell(ubiv1, size);
-    end = clock();
-    vremya[k] = (double)(end - start) / CLOCKS_PER_SEC;
-    k++;
-
-    start = clock();
-    shell(vozubiv1, size);
-    end = clock();
-    vremya[k] = (double)(end - start) / CLOCKS_PER_SEC;
-    k++;
-
-    //----------------------------------
-
-    start = clock();
-    qs(randarr2, 0, size - 1);
-    end = clock();
-    vremya[k] = (double)(end - start) / CLOCKS_PER_SEC;
-    k++;
-
-    start = clock();
-    qs(vozrast2, 0, size - 1);
-    end = clock();
-    vremya[k] = (double)(end - start) / CLOCKS_PER_SEC;
-    k++;
-
-    start = clock();
-    qs(ubiv2, 0, size - 1);
-    end = clock();
-    vremya[k] = (double)(end - start) / CLOCKS_PER_SEC;
-    k++;
-
-    start = clock();
-    qs(vozubiv2, 0, size - 1);
-    end = clock();
-    vremya[k] = (double)(end - start) / CLOCKS_PER_SEC;
-    k++;
-
-    //----------------------------------
-
-    start = clock();
-    qsort(randarr3, size, sizeof(int), compare);
-    end = clock();
-    vremya[k] = (double)(end - start) / CLOCKS_PER_SEC;
-    k++;
-
-    start = clock();
-    qsort(vozrast3, size, sizeof(int), compare);
-    end = clock();
-    vremya[k] = (double)(end - start) / CLOCKS_PER_SEC;
-    k++;
-
-    start = clock();
-    qsort(ubiv3, size, sizeof(int), compare);
-    end = clock();
-    vremya[k] = (double)(end - start) / CLOCKS_PER_SEC;
-    k++;
-
-    start = clock();
-    qsort(vozubiv3, size, sizeof(int), compare);
-    end = clock();
-    vremya[k] = (double)(end - start) / CLOCKS_PER_SEC;
-
-    printf("       |  rand |  voz  |   ub  |   vu  |\n----------------------------\n");
-    printf(" shell | %3.3lf | %3.3lf | %3.3lf | %3.3lf |\n----------------------------\n", vremya[0], vremya[1], vremya[2], vremya[3]);
-    printf("   qs  | %3.3lf | %3.3lf | %3.3lf | %3.3lf |\n----------------------------\n", vremya[4], vremya[5], vremya[6], vremya[7]);
-    printf(" qsort | %3.3lf | %3.3lf | %3.3lf | %3.3lf |\n----------------------------\n", vremya[8], vremya[9], vremya[10], vremya[11]);
-
-    //print(&randarr1[size], size);
-    //printf("\n");
-    //print(&vozrast1[size], size);
-    //printf("\n");
-    //print(&ubiv1[size], size);
-    //printf("\n");
-    //print(&vozubiv1[size], size);
-    //printf("\n");
+    printf("       |  rand |  voz  |  ub   |  vu   |\n-----------------------------------------\n");
+    printf(" shell | %3.3lf | %3.3lf | %3.3lf | %3.3lf |\n-----------------------------------------\n", vremya[0], vremya[1], vremya[2], vremya[3]);
+    printf("   qs  | %3.3lf | %3.3lf | %3.3lf | %3.3lf |\n-----------------------------------------\n", vremya[4], vremya[5], vremya[6], vremya[7]);
+    printf(" qsort | %3.3lf | %3.3lf | %3.3lf | %3.3lf |\n-----------------------------------------\n", vremya[8], vremya[9], vremya[10], vremya[11]);
 
     return 0;
 }
